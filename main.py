cd6934a74a014c2dbbb0481a5529c055b3afd8cb
Viktor Ericson <vcen21@student.aau.dk>
2022-02-23 16:08:48 +0100

    shit

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
+@app.route('/login',methods=['POST',"GET"])
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run()
